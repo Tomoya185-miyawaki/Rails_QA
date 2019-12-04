b0fd01cd81caf62cdfd2657d98512c7a470cd631
Tomoya Miyawaki <ec2-user@ip-172-31-35-192.ap-northeast-1.compute.internal>
2019-12-04 14:47:32 +0000

    質問を作成できるようにする

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,23 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: "success!"
+    else
+      flash[:alert] = 'Save Error!'
+    end
   end
 
   def edit
   end
+  
+  private 
+    def question_params
+      params.require(:question).permit(:name, :title, :tent)
+    end
 end
